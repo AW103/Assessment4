bb9e6a9e2559aee1c5f8f30afa0dc6a258d181ff
Andrea Walsh <andreawalsh@Carters1MacBook.attlocal.net>
2022-04-28 17:41:21 -0500

    adding connection

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"))
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
 app.get("/api/quote", getQuote);
@@ -17,4 +20,9 @@ app.post("/api/form", createPositive);
 app.delete("/api/formDelete/:id", deletePositive);
 app.put("/api/updatePositive/:id", updatePositive)
 
-app.listen(4000, () => console.log("Server running on 4000"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server rocking out on ${port}`)
+})
+
