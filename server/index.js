85534078c85ea2507a11c1857e14e36e8f5af59d
Andrea Walsh <andreawalsh@Carters1MacBook.attlocal.net>
2022-05-13 15:50:47 -0500

    test

--- server/index.js
+++ server/index.js
@@ -15,12 +15,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-// app.use(session({
-//     secret: secret,
-//     saveUninitialized:true,
-//     resave: false,
-//     cookie: { maxAge: 6000 }
-// }))
+app.use(session({
+    secret: secret,
+    saveUninitialized:true,
+    resave: false,
+}))
 app.use("/styles", express.static(path.join(__dirname,"../public/styles.css")));
 app.use("/frontend", express.static(path.join(__dirname,"../public/main.js")));
 
