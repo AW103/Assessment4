7aa6887bb2486b1bf6b060e2207e41f33b62fc29
Andrea Walsh <andreawalsh@Carters1MacBook.attlocal.net>
2022-05-13 15:47:55 -0500

    test

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(session({
     secret: secret,
